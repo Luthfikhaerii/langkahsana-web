a232e00be59bb0790c460c740556a158570712f3
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-06-18 13:49:26 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,21 +18,21 @@ export default function Home() {
       {/* ABOUT */}
 
       <div className="flex md:flex-nowrap sm:flex-nowrap flex-wrap items-start w-full">
-        <div className="h-full w-full">
+        <div className="h-full w-full md:bg-transparent sm:bg-transparent bg-coklat-muda">
         <Image src={"/images/about_home.webp"} width={600} height={500} alt="foto" className=" polygon-md md:inline-block sm:inline-block hidden w-[100%] h-[300px]"/>
-        <Image src={"/images/about_home.webp"} width={0} height={0} alt="foto" className=" polygon-xs md:hidden sm:hidden inline-block"/>
+        <Image src={"/images/about_home.webp"} width={600} height={500} alt="foto" className=" polygon-xs md:hidden sm:hidden inline-block w-[100%] h-[300px]"/>
         </div>
         <div className="w-[150%] h-full">
-          <div className=" md:p-10 sm:p-14 p-8 bg-coklat-muda md:h-[230px] sm:h-[230px] h-full">
+          <div className=" md:p-10 sm:p-8 p-8 bg-coklat-muda md:h-[230px] sm:h-[230px] h-full">
             <h2 className="md:text-4xl sm:text-3xl text-2xl md:text-left sm:text-left text-center font-bold mb-5 text-coklat-tua">KITA LANGKAHSANA</h2>
-            <p className="text-coklat-tua md:text-sm sm:text-lg text-lg md:text-left sm:text-left text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab rem sit nesciunt consequatur vel labore optio harum error eaque magni nobis libero modi nihil, iure totam doloribus debitis reprehenderit incidunt maxime</p>
+            <p className="text-coklat-tua md:text-sm sm:text-sm text-sm md:text-left sm:text-left text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab rem sit nesciunt consequatur vel labore optio harum error eaque magni nobis libero modi nihil, iure totam doloribus debitis reprehenderit incidunt maxime</p>
           </div>
           <div className="w-full h-20 flex justify-center items-center">
             <Link href={"#"}>
               <div>
-                <p className="md:text-2xl sm:text-2xl text-lg font-semibold text-coklat-2tua">READ MORE</p>
+                <p className="md:text-2xl sm:text-xl text-lg font-semibold text-coklat-2tua">READ MORE</p>
               </div>
-              <div className="md:h-1 sm:h-1 h-[2px] w-full bg-coklat-2tua rounded-lg"></div>
+              <div className="md:h-1 sm:h-[2px] h-[2px] w-full bg-coklat-2tua rounded-lg"></div>
             </Link>
           </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,34 @@
 import Link from "next/link"
 import Image from "next/image"
 import { Chivo_Mono } from 'next/font/google'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const inter = Chivo_Mono({ subsets: ['latin'], weight: "700" });
 
 export default function Navbar() {
     const [on, setOn] = useState(false)
+    const [current, setCurrent] = useState(0)
+    const [before, setBefore] = useState(0)
+    const [direction, setDirection] = useState('up')
+
+    useEffect(() => {
+        let lastscroll = window.scrollY;
+        const listen = () => {
+            let currentscroll = window.scrollY;
+            if (currentscroll < lastscroll) {
+                setDirection('up')
+            }else{
+                setDirection('down')
+            }
+            lastscroll = currentscroll
+        }
+        window.addEventListener('scroll', listen)
+
+        return () => { window.removeEventListener('scroll', listen) }
+    }, [])
 
     return (
         <div className={inter.className}>
-            <div className="fixed z-40">
+            <div className={`${direction=='up'?'top-0 ease-in-out duration-700':on?'top-0':'-top-16 ease-in-out duration-700'} fixed z-40`}>
                 <div className="grid grid-cols-2 h-16 w-screen bg-[#CA9271]">
                     <div className="flex items-center md:ml-14 sm:ml-14 ml-4">
                         <Image src={"/images/logo.jpeg"} alt={"logo"} width={50} height={50} />
@@ -26,11 +45,11 @@ export default function Navbar() {
                         </div>
                         <div className="md:inline-block sm:block xl:block hidden w-full justify-end ">
                             <div className="flex justify-end">
-                                <div className="grid md:grid-cols-4 md:gap-5 md:justify-items-center items-center w-8/12 mr-14">
-                                    <Link href={"/"}><p className="text-white text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Home</p></Link>
-                                    <Link href={"/"}><p className="text-white text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Journal</p></Link>
-                                    <Link href={"/"}><p className="text-white text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">About</p></Link>
-                                    <Link href={"/"}><p className="text-white text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Contact</p></Link>
+                                <div className="grid md:grid-cols-4 sm:grid-cols-4 md:gap-5 md:justify-items-center items-center md:w-8/12 sm:w-10/12 w-8/12 mr-14">
+                                    <Link href={"/"}><p className="text-white  md:text-base  sm:text-sm text-base px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Home</p></Link>
+                                    <Link href={"/"}><p className="text-white md:text-base  sm:text-sm text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Journal</p></Link>
+                                    <Link href={"/"}><p className="text-white md:text-base  sm:text-sm text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">About</p></Link>
+                                    <Link href={"/"}><p className="text-white md:text-base  sm:text-sm text-base  px-7 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] hover:text-gray-400">Contact</p></Link>
                                 </div>
                             </div>
 
@@ -38,7 +57,7 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
-            <div className={` md:hidden sm:hidden xl:hidden fixed z-30 ${on ? "top-[60px] fadein" : "ease-in-out top-[-130px] duration-700"} w-screen  bg-[#CA9271]`}>
+            <div className={` md:hidden sm:hidden xl:hidden fixed z-30 ${on ? "top-[60px] fadein" : "ease-in top-[-200px] duration-700"} w-screen  bg-[#CA9271]`}>
                 <div className="flex justify-center">
                     <div className="grid grid-cols-1 justify-items-center items-center">
                         <Link href={"/"}><p className="border-b-[1px] hover:text-gray-400 py-2 w-screen text-center text-white text-base [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">Home</p></Link>
@@ -47,7 +66,7 @@ export default function Navbar() {
                         <Link href={"/"}><p className=" hover:text-gray-400 py-2 w-screen text-center text-white text-base [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">Contact</p></Link>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     )
