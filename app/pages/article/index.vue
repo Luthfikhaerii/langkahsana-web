2bf742c6d5e778e4379ffb47f5ed7464585ac017
luthfikhaerii <luthfikhaeri00@gmail.com>
2025-09-26 21:32:36 +0700

    realtime search

--- app/pages/article/index.vue
+++ app/pages/article/index.vue
@@ -2,23 +2,29 @@
 import axios from 'axios';
 import { VueAwesomePaginate } from 'vue-awesome-paginate';
 
+const env = useRuntimeConfig()
 const page = ref(1)
 const limit = 12
 const search = ref('')
-
-const sortType = ref('newest') // default pilihan
-const env = useRuntimeConfig()
+const sort = ref('desc')
 
 const options = [
-    { value: 'newest', label: 'Newest' },
-    { value: 'oldest', label: 'Oldest' },
-    { value: 'a-z', label: 'A → Z' },
-    { value: 'z-a', label: 'Z → A' }
+    { value: 'desc', label: 'Newest' },
+    { value: 'esc', label: 'Oldest' }
 ]
 
 const onClickHandler = (current: number) => {
     page.value = current
 };
+
+const onSearchHandler = (e: Event) => {
+    console.log('jalan aja')
+    setTimeout(() => {
+        console.log('masuk')
+        const target = e.target as HTMLInputElement
+        search.value = target.value
+    }, 1000);
+}
 
 const { data: articles, error } = await useAsyncData(
     () => `articles-${page}-${search}`,
@@ -57,13 +63,13 @@ if (error.value) {
             </div>
         </div>
         <div class="w-full mb-8 flex justify-between items-end max-w-screen-xl mx-auto">
-            <input type="text" placeholder="search..."
+            <input type="text" placeholder="search..." @input="e=>onSearchHandler(e)"
                 class="w-96 px-4 py-2 h-10  border rounded-xl border-gray-400 shadow-md" />
             <div class="w-48">
                 <label for="sort" class="block mb-2 text-sm font-medium text-gray-700">
                     Sort by
                 </label>
-                <select id="sort" v-model="sortType"
+                <select id="sort" v-model="sort"
                     class="w-full px-4 py-2 border rounded-xl border-gray-400 shadow-md rounded-lg border-gray-300 focus:border-blue-500 focus:ring-blue-500 text-sm">
                     <option v-for="option in options" :key="option.value" :value="option.value">
                         {{ option.label }}
@@ -72,22 +78,24 @@ if (error.value) {
             </div>
         </div>
         <div class="grid grid-cols-4 gap-10 max-w-screen-xl mx-auto">
-            <CardArticle v-for="value in articles.data" :key="value.id" :title="value.title" :description="value.description" :date="new Date(value.date)" :image="value.image" />
+            <CardArticle v-for="value in articles.data" :key="value.id" :title="value.title"
+                :description="value.description" :date="new Date(value.date)" :image="value.image" />
         </div>
         <div class="mt-16 w-full flex justify-center">
-            <VueAwesomePaginate :total-items="articles?.total" :items-per-page="12" :max-pages-shown="3" v-model="page" @click="onClickHandler" />
+            <VueAwesomePaginate :total-items="articles?.total" :items-per-page="12" :max-pages-shown="3" v-model="page"
+                @click="onClickHandler" />
         </div>
     </section>
 </template>
 
 <style>
-  .pagination-container {
+.pagination-container {
     display: flex;
 
     column-gap: 10px;
-  }
+}
 
-  .paginate-buttons {
+.paginate-buttons {
     height: 40px;
 
     width: 40px;
@@ -101,21 +109,21 @@ if (error.value) {
     border: 1px solid rgb(217, 217, 217);
 
     color: black;
-  }
+}
 
-  .paginate-buttons:hover {
+.paginate-buttons:hover {
     background-color: #d8d8d8;
-  }
+}
 
-  .active-page {
+.active-page {
     background-color: #3498db;
 
     border: 1px solid #3498db;
 
     color: white;
-  }
+}
 
-  .active-page:hover {
+.active-page:hover {
     background-color: #2988c8;
-  }
+}
 </style>
